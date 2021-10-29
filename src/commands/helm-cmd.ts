8b24069688dbeab2d5bf1937c3baf5b3726bf39e
Olivier GROSJEANNE <developer@grosjeanne.name>
2021-10-29 09:33:42 +0200

    getAliasedGlobalFlags with a generic rule

--- src/commands/helm-cmd.ts
+++ src/commands/helm-cmd.ts
@@ -1,13 +1,16 @@
-import { GlobalFlags } from '../types';
-import install from './install-cmd';
-import list from './list-cmd';
-import repo from './repo-cmd';
+import { GlobalFlags } from "../types";
+import install from "./install-cmd";
+import list from "./list-cmd";
+import repo from "./repo-cmd";
 
-export const getAliasedGlobalFlags = ({
-  namespace = '',
-}: GlobalFlags = {}) => ({
-  '--namespace': namespace,
-});
+export const getAliasedGlobalFlags = (flags: GlobalFlags) => {
+  const result: { [key: string]: string } = {};
+  for (const [key, value] of Object.entries(flags)) {
+    const k: string = "--" + key;
+    result[k] = value as string;
+  }
+  return result;
+};
 
 export default {
   install,
